6d3f8168886a11300806f365c0570806bcce906f
Matheus ramalho da silva <matheusramalhoss23@gmail.com>
2026-05-20 10:13:57 -0300

    Feat: Splash screen

--- app/src/main/java/com/matheusramalho/joiaapp2026/MainActivity.kt
+++ app/src/main/java/com/matheusramalho/joiaapp2026/MainActivity.kt
@@ -2,12 +2,20 @@ package com.matheusramalho.joiaapp2026
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.os.Looper
+import android.content.Intent
+import com.matheusramalho.joiaapp2026.view.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
